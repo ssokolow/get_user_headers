856fb2ffd2ecd24428f971a204f1487746498140
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-07-16 12:36:08 -0400

    Add tests for everything in UserHeaderGetter testable without _get_uncached

--- test_get_user_headers.py
+++ test_get_user_headers.py
@@ -7,7 +7,7 @@ from __future__ import (absolute_import, division, print_function,
 __author__ = "Stephan Sokolow (deitarion/SSokolow)"
 __license__ = "MIT"
 
-import datetime, math
+import datetime, math, os, random, shutil, sqlite3, tempfile, unittest
 
 import get_user_headers
 
@@ -70,3 +70,124 @@ def test_timestamp_epoch():
 def test_timestamp_recent():
     """_timestamp(): round-trips correctly at a typical time"""
     check_timestamp_roundtrip(1468673923)
+
+class UserHeaderGetterTests(unittest.TestCase):
+    """Tests for UserHeaderGetter"""
+    test_data = {
+        'Foo': 'Bar',
+        'baz': 'quux',
+        'SPAM': 'EGGS',
+    }
+
+    test_headers = {x: 'foo{}'.format(random.random()) for x in
+                    list(get_user_headers.UserHeaderGetter.safe_headers) +
+                    list(get_user_headers.UserHeaderGetter.unsafe_headers) +
+                    ['X-Testing-{}'.format(random.random())]}
+
+    def setUp(self):
+        """Initialize test space on filesystem"""
+        self.tempdir = tempfile.mkdtemp(prefix='nosetests-')
+        print(self.tempdir)
+        self.getter = get_user_headers.UserHeaderGetter(self.tempdir)
+
+    def tearDown(self):
+        """Remove test space on filesystem"""
+        shutil.rmtree(self.tempdir)
+
+    # TODO: Tests still to be written:
+    # - __init__(path=None)
+    # - get_all(headers=None)
+    # - get_safe(headers=None)
+    # - All uses of the skip_cache attribute
+
+    def test_access_denied(self):
+        """UserHeaderGetter: 'access denied' in __init__"""
+
+        nonexist = '/w409vrporpe'
+        self.assertFalse(os.path.exists(nonexist))
+
+        readonly = 'C:\\' if os.name == 'nt' else '/'
+        self.assertTrue(os.path.exists(readonly))
+
+        # os.makedirs failure
+        self.assertRaises(OSError, get_user_headers.UserHeaderGetter, nonexist)
+
+        # sqlite3.connect failure
+        self.assertRaises(sqlite3.OperationalError,
+                          get_user_headers.UserHeaderGetter, readonly)
+
+    def test_clear_expired(self):
+        """UserHeaderGetter: clear_expired() functions properly"""
+        self.assertIsNone(self.getter._get_cache())
+        self.getter._save_cache(self.test_data.copy())
+
+        self.getter.clear_expired()
+        self.assertEqual(self.getter._get_cache(), self.test_data,
+                         "Shouldn't expire freshly-added entries")
+
+        # XXX: Do I want a less "DIY hack"-ish solution here?
+        real_dt = datetime.datetime
+        try:
+            class MockDateTime(object):
+                @staticmethod
+                def now():
+                    return real_dt.now() + (
+                        self.getter.cache_timeout +
+                        datetime.timedelta(seconds=1))
+
+            datetime.datetime = MockDateTime
+            self.getter.clear_expired()
+        finally:
+            datetime.datetime = real_dt
+
+        self.assertFalse(self.getter._get_cache(),
+                         "Failed to remove stale entries")
+
+    def test_get_all_as_filter(self):
+        """UserHeaderGetter: get_all() properly filters input"""
+        results = self.getter.get_all(self.test_headers.copy())
+
+        #TODO: Test the correctness of the case-insensitivity
+        for key in results.keys():
+            self.assertNotIn(key, self.getter.unsafe_headers,
+                "Unsafe headers in get_all() output")
+
+        # Verify the filtering process didn't modify the key=value pairs
+        for key, value in results.items():
+            self.assertEqual(value, self.test_headers.get(key,
+                                                          random.random()))
+
+    def test_get_safe_as_filter(self):
+        """UserHeaderGetter: get_safe() properly filters input"""
+        results = self.getter.get_safe(self.test_headers.copy())
+
+        #TODO: Test the correctness of the case-insensitivity
+        self.assertEqual(set(results.keys()), set(self.getter.safe_headers),
+            "Unknown headers in get_safe() output")
+
+
+        # Verify the filtering process didn't modify the key=value pairs
+        for key, value in results.items():
+            self.assertEqual(value, self.test_headers.get(key,
+                                                          random.random()))
+
+    def test_parent_dir_exists(self):
+        """UserHeaderGetter: cache directory already exists"""
+        get_user_headers.UserHeaderGetter(self.tempdir)
+
+    def test_storage(self):
+        """UserHeaderGetter: basic functionality of SQLite-wrapping calls"""
+        self.assertIsNone(self.getter._get_cache())
+
+        stored = self.test_data.copy()
+        self.getter._save_cache(stored)
+        self.assertEqual(stored, self.test_data)
+
+        retrieved = self.getter._get_cache()
+        self.assertEqual(retrieved, stored)
+
+        retrieved_again = self.getter._get_cache()
+        self.assertEqual(retrieved_again, retrieved)
+
+        self.assertEqual(retrieved_again, self.test_data,
+                        "Error introduced somewhere in round-tripping")
